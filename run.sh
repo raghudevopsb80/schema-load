c5ebaabf4fa91a3a77a9cf6c170afd4666f6879b
Raghu K <raghuk.vit@gmail.com>
2024-08-27 06:51:02 +0530

    RoboShop Kubernetes

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@ mkdir /app
 git clone $APP_GIT_URL /app
 
 if [ "$DB_TYPE" == "mongo" ]; then
-  for file in $SHEMA_FILES ; do
+  for file in $SCHEMA_FILES ; do
     mongosh --host $DB_HOST </app/db/$file.js
   done
 fi
 
 if [ "$DB_TYPE" == "mysql" ]; then
-  for file in $SHEMA_FILES ; do
+  for file in $SCHEMA_FILES ; do
     mysql -h $DB_HOST -u$DB_USER -p$DB_PASS < /app/db/$file.sql
   done
 fi
